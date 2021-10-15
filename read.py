5baccec05a537301e9bda6aa8b21eebce4bcb52d
just maxime <jm002743@rtbase2lin.campus.unice.fr>
2021-10-15 15:24:39 +0200

    Modified most of the way the program works

--- read.py
+++ read.py
@@ -21,61 +21,31 @@ def parse(inputFile):
             #the file is named "opened", in the loop each line is named line
             processedLine={}
 
-            cutLine=re.search("(([0-9]*\.){3}[0-9]*).*\[(.*)\].*\"([A-Z]+) .+\" ([0-9]{3}) (.*) \"(.+)\" \"(.+)\"",line)
+            cutLine=re.search("(([0-9]*\.){3}[0-9]*).*\[(.*)\].*\"([A-Z]+) .+\" ([0-9]{3}) (.*) \"(.+)\" \"(.+)\"?",line)
 
             try:
                 processedLine['remote_ip']=cutLine.group(1)
                 #Dictionnary appending works, now we need to generate the whole thing
-            except:
-                print("IP is wrong in the line : returning Broken or Empty")
-                processedLine['remote_ip']="Broken or Empty"
-                count=count+1
             
-            try:
                 processedLine['time']=cutLine.group(3)
-            except:
-                print("Time is wrong in the line : returning Broken or Empty")
-                processedLine['time']="Broken or Empty"
-                count=count+1
 
-            try:
                 processedLine['request']=cutLine.group(4)
-            except:
-                print("Request is wrong in the line : returning Broken or Empty")
-                processedLine['request']="Broken or Empty"
-                count=count+1
 
-            try:
                 processedLine['response']=cutLine.group(5)
-            except:
-                print("Response is wrong in the line : returning Broken or Empty")
-                processedLine['response']="Broken or Empty"
-                count=count+1
 
-            try:
                 processedLine['bytes']=cutLine.group(6)
-            except:
-                print("Bytes is wrong in the line : returning Broken or Empty")
-                processedLine['bytes']="Broken or Empty"
-                count=count+1
 
-            try:
                 processedLine['referrer']=cutLine.group(7)
-            except:
-                print("Referrer is wrong in the line : returning Broken or Empty")
-                processedLine['referrer']="Broken or Empty"
-                count=count+1
 
-            try:
                 processedLine['agent']=cutLine.group(8)
+
+                result.append(processedLine)
             except:
-                print("Agent is wrong in the line : returning Broken or Empty")
-                processedLine['agent']="Broken or Empty"
+                result.append("Broken")
                 count=count+1
 
                 #processedLine[]=re.search(,line).group()
-            result.append(processedLine)
     
-    print(str(count) + " defective or empty elements have been reported")
+    print(str(count) + " defective lines have been reported")
 
     return result
