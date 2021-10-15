5efe020605374c9612818355c2918f858734e676
just maxime <jm002743@rtbase2lin.campus.unice.fr>
2021-10-15 14:31:15 +0200

    Making new branch to remake regexs

--- read.py
+++ read.py
@@ -11,10 +11,18 @@ import re
 #Dictionnary reset at each line
     
 #re.search("^([^ ]+ ){3}\[[^ ]+ [^ ]+\] \"[A-Z]+ /.+ HTTP\/[0-9]\.[0-9]\" ([0-9]+ ){2}\"https?://.+\" \".+\"",expression).group()
-#this regex was created to test the fitting of whole linesm ended up unused
+#this regex was created to test the fitting of whole lines and ended up unused
 
 def parse(inputFile):
     count=0
+    ipErr=0
+    timeErr=0
+    reqErr=0
+    respErr=0
+    bytErr=0
+    refErr=0
+    agErr=0
+
     result=[]
     with open(inputFile, "r") as opened:
         for line in opened:
@@ -25,55 +33,64 @@ def parse(inputFile):
                 processedLine['remote_ip']=re.search("([0-9]{1,3}\.){3}[0-9]{1,3}",line).group()
                 #Dictionnary appending works, now we need to generate the whole thing
             except:
-                print("IP is wrong in the line : returning Broken or Empty")
                 processedLine['remote_ip']="Broken or Empty"
                 count=count+1
+                ipErr=ipErr+1
             
             try:
                 processedLine['time']=re.search("[0-9]{2}\/[A-Z][a-z]*\/[0-9]*(:[0-9]{2}){3} [\+\-][0-9]{4}",line).group()
             except:
-                print("Time is wrong in the line : returning Broken or Empty")
                 processedLine['time']="Broken or Empty"
                 count=count+1
+                timeErr=timeErr+1
 
             try:
                 processedLine['request']=re.search("\"[A-Z]{3,} ",line).group()[1:-1]
             except:
-                print("Request is wrong in the line : returning Broken or Empty")
                 processedLine['request']="Broken or Empty"
                 count=count+1
+                reqErr=reqErr+1
 
             try:
                 processedLine['response']=re.search("\" [0-9]{3}",line).group()[2:]
             except:
-                print("Response is wrong in the line : returning Broken or Empty")
                 processedLine['response']="Broken or Empty"
                 count=count+1
+                respErr=respErr+1
 
             try:
                 processedLine['bytes']=re.search("[0-9]+ \"",line).group()[:-2]
             except:
-                print("Bytes is wrong in the line : returning Broken or Empty")
                 processedLine['bytes']="Broken or Empty"
                 count=count+1
+                bytErr=bytErr+1
 
             try:
                 processedLine['referrer']=re.search("\"https?:\/(/[^ \/]+)+\/?\"",line).group()[1:-1]
             except:
-                print("Referrer is wrong in the line : returning Broken or Empty")
                 processedLine['referrer']="Broken or Empty"
                 count=count+1
+                refErr=refErr+1
 
             try:
-                processedLine['agent']=re.search("\"[A-Z]{1}[a-z]+\/.+\..+( .+)*\"",line).group()[1:-1]
+                processedLine['agent']=re.search("\"[A-Z]{1}([a-z]|[A-Z]| )+([a-z]|[A-Z])+.([0-9]+(\.| ))+.*\"",line).group()[1:-1]
             except:
-                print("Agent is wrong in the line : returning Broken or Empty")
                 processedLine['agent']="Broken or Empty"
                 count=count+1
+                agErr=agErr+1
+
+                print(line)
 
                 #processedLine[]=re.search(,line).group()
             result.append(processedLine)
     
-    print(str(count) + " defective or empty elements have been reported")
+    print(str(count) + " defective or empty elements have been reported :")
+    print(str(ipErr) + " IP adresses fields")
+    print(str(timeErr) + " time fields")
+    print(str(reqErr) + " request type fields")
+    print(str(respErr) + " response code fields")
+    print(str(bytErr) + " bytes quantity fields")
+    print(str(refErr) + " referrer address fields")
+    print(str(agErr) + " agent fields")
 
     return result
